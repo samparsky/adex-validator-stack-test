b06cde02222d16000d38622c75b1adc17eaf83cb
Omidiora Samuel <>
2019-01-15 10:18:01 +0100

    sendEvents: fix authorization error

--- test/entity.js
+++ test/entity.js
@@ -1,11 +1,15 @@
 const test = require("blue-tape");
 const { MongoMemoryServer } = require("mongodb-memory-server");
-const { getMongo, connect } = require("../stack/db");
+const { MongoClient } = require('mongodb')
+
 // const request = require("supertest");
 const sentry = require("../stack/bin/sentry")
-const worker = require("../stack/bin/validatorWorker")
+// const worker = require("../stack/bin/validatorWorker")
 const util = require("util")
 const exec = util.promisify(require("child_process").exec)
+const { spawn } = require("child_process")
+const fetch = require("node-fetch");
+
 // const request = require("request")
 
 // const mongod = new MongoMemoryServer();
@@ -13,7 +17,9 @@ const exec = util.promisify(require("child_process").exec)
 // console.log({uri})
 
 let dbUrl
-
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 async function setupDb(dbName){
     if(dbUrl) {
         return dbUrl
@@ -25,13 +31,12 @@ async function setupDb(dbName){
     return dbUrl
 }
 
-async function seedDatabase(id, uri){
-    process.env.DB_MONGO_URL = uri
-    process.env.DB_MONGO_NAME = id
-
-    await connect();
-
-    const db = getMongo()
+async function seedDatabase(id, uri, channel="awesomeTestChannel"){
+    console.log({id})
+    console.log({uri})
+    
+    const mongoClient = await MongoClient.connect(uri || 'mongodb://localhost:27017', { useNewUrlParser: true })
+    const db = mongoClient.db(id)
 
     db.collection("sessions").insertOne({ _id: 'x8c9v1b2', uid: 'awesomeTestUser' })
 
@@ -40,8 +45,8 @@ async function seedDatabase(id, uri){
 
     db.collection("channels").insertOne({
         // @TODO: document schema
-        _id: 'awesomeTestChannel',
-        id: 'awesomeTestChannel',
+        _id: channel,
+        id: channel,
         status: 'live',
         // @TODO: ERC20 addr
         depositAsset: 'DAI',
@@ -56,11 +61,21 @@ async function seedDatabase(id, uri){
     })
 }
 
-async function sendMessages(port, channel="awesomeTestChannel"){
+async function sendEvents(port, channel="awesomeTestChannel"){
     const url = `http://localhost:${port}/channel/${channel}/events`
-    const body = {"events": [{"type": "IMPRESSION", "publisher": "myAwesomePublisher"}]}
+    const body = JSON.stringify({"events": [{"type": "IMPRESSION", "publisher": "myAwesomePublisher"}]})
 
     // send to leader
+    const response = await fetch(url, {
+        headers: {
+            "authorization": "Bearer x8c9v1b2",
+            "content-type": "application/json"
+        },
+        body,
+        method: "POST"
+    });
+
+    console.log({response})
     
     // send to follower
 
@@ -72,48 +87,76 @@ async function sendMessages(port, channel="awesomeTestChannel"){
 async function _setupSentry(id, dbUrl, port){
     await seedDatabase(id, dbUrl)
 
-    const cmd = `DB_MONGO_NAME=${id} DB_MONGO_URL=${dbUrl} PORT=${port} node /Users/Samparsky/Sites/nodejs/adex-validator-stack-test/stack/bin/sentry.js --adapter=dummy --dummyIdentity=${id} &`
-    const { stdout, stderr } = await exec(cmd)
+    console.log("executing command")
 
-    console.log({ stdout })
-    console.log({ stderr })
+    const cmd = `DB_MONGO_NAME=${id} DB_MONGO_URL=${dbUrl || ''} PORT=${port} nohup node /Users/Samparsky/Sites/nodejs/adex-validator-stack-test/stack/bin/sentry.js --adapter=dummy --dummyIdentity=${id}`
+    console.log({cmd})
+    const spawnR = spawn(cmd, {
+        stdio: 'inherit',
+        shell: true,
+        detached: true
+    })
+    console.log({spawnR})
+
+    console.log("hello world")
 }
 
 async function _setupWorker(id, dbUrl){
-    await seedDatabase(id, dbUrl)
+    const cmd = `DB_MONGO_NAME=${id} DB_MONGO_URL=${dbUrl || ''} nohup node /Users/Samparsky/Sites/nodejs/adex-validator-stack-test/stack/bin/validatorWorker.js --adapter=dummy --dummyIdentity=${id} >> /dev/null &`
+    const rSpawn = await spawn(cmd, {
+        stdio: 'inherit',
+        shell: true,
+        detached: true
+    })
 
-    const cmd = `DB_MONGO_NAME=${id} DB_MONGO_URL=${dbUrl} node /Users/Samparsky/Sites/nodejs/adex-validator-stack-test/stack/bin/validatorWorker.js --adapter=dummy --dummyIdentity=${id} &`
-    const { stdout, stderr } = await exec(cmd)
-
-    console.log({ stdout })
-    console.log({ stderr })
+    console.log({ rSpawn })
+    // console.log({ stderr })
 }
 
 
 async function setupLeader(){
     // setup db
-    const uri = await setupDb();
+    // const uri = await setupDb();
+    const port = 8005
     // run sentry
-    await _setupSentry("awesomeLeader", "", 8009)
+    console.log("setup sentry") 
+
+    await _setupSentry("awesomeLeader", "", port)
+    console.log('Taking a break...');
+    await sleep(2000);
+    console.log('Two seconds later');
     // setup work
     await _setupWorker("awesomeLeader", "")
+    // send messages
+    console.log("send messages") 
+    await sendEvents(port)
+
+
+
 }
 
 async function setupFollower(){
     const uri = await setupDb();
+    const port = 8006
+
     // run sentry
-    await _setupSentry("awesomeFollower", "", 8010)
-    // setup work
+    await _setupSentry("awesomeFollower", "", port)
+    console.log('Taking a break...');
+    await sleep(2000);
+    console.log('Two seconds later');
+    // // setup work
     await _setupWorker("awesomeFollower", "")
+
+    await sendEvents(port)
 }
 
 // Working Scenarios
 test("Should deposit into the channel", async(t) => {
     // setup database
     // seed database
-    await setupLeader();
-    await setupFollower();
-    await sendMessages();
+    // await setupLeader();
+    // await setupFollower();
+    // await sendMessages();
 
     // confirm follower & leader received message
 
@@ -122,6 +165,8 @@ test("Should deposit into the channel", async(t) => {
 test("Leader signs a valid state, followers should detect and sign", async(t) => {
     // start leader
     // start follower
+    await setupLeader();
+    await setupFollower();
 })
 
 test("Should send events to the setup", async(t) => {
