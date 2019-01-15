586a7207478fb4dd1775efa3c20f484dde66ede4
Omidiora Samuel <>
2019-01-15 13:11:02 +0100

    added: dockerfile

--- test/entity.js
+++ test/entity.js
@@ -7,9 +7,20 @@ const sentry = require("../stack/bin/sentry")
 // const worker = require("../stack/bin/validatorWorker")
 const util = require("util")
 const exec = util.promisify(require("child_process").exec)
-const { spawn } = require("child_process")
+const { spawn, fork } = require("child_process")
 const fetch = require("node-fetch");
 
+let pids = []
+
+const exitHandler =  () => {
+    console.log(`killing processes with id ${pids.join(" ,")}`)
+    pids.map((id) => process.kill(parseInt(`-${id}`)))
+    console.log("Success")
+    // process.exit(0)
+}
+
+// process.on('exit', exitHandler)
+process.on('SIGINT', exitHandler)
 // const request = require("request")
 
 // const mongod = new MongoMemoryServer();
@@ -20,6 +31,9 @@ let dbUrl
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+
+
 async function setupDb(dbName){
     if(dbUrl) {
         return dbUrl
@@ -63,7 +77,8 @@ async function seedDatabase(id, uri, channel="awesomeTestChannel"){
 
 async function sendEvents(port, channel="awesomeTestChannel"){
     const url = `http://localhost:${port}/channel/${channel}/events`
-    const body = JSON.stringify({"events": [{"type": "IMPRESSION", "publisher": "myAwesomePublisher"}]})
+    // const body = JSON.stringify({"events": [{"type": "IMPRESSION", "publisher": "myAwesomePublisher"}]})
+        const body = JSON.stringify({"events": [{"type": "CLICK", "publisher": "myAwesomePublisher"}]})
 
     // send to leader
     const response = await fetch(url, {
@@ -89,27 +104,28 @@ async function _setupSentry(id, dbUrl, port){
 
     console.log("executing command")
 
-    const cmd = `DB_MONGO_NAME=${id} DB_MONGO_URL=${dbUrl || ''} PORT=${port} nohup node /Users/Samparsky/Sites/nodejs/adex-validator-stack-test/stack/bin/sentry.js --adapter=dummy --dummyIdentity=${id}`
+    const cmd = `DB_MONGO_NAME=${id} DB_MONGO_URL=${dbUrl || ''} PORT=${port} nohup node /Users/Samparsky/Sites/nodejs/adex-validator-stack-test/stack/bin/sentry.js --adapter=dummy --dummyIdentity=${id} >> ${id}.out`
     console.log({cmd})
     const spawnR = spawn(cmd, {
         stdio: 'inherit',
         shell: true,
         detached: true
     })
-    console.log({spawnR})
+    pids.push(spawnR.pid)
+    // console.log({spawnR})
 
     console.log("hello world")
 }
 
 async function _setupWorker(id, dbUrl){
-    const cmd = `DB_MONGO_NAME=${id} DB_MONGO_URL=${dbUrl || ''} nohup node /Users/Samparsky/Sites/nodejs/adex-validator-stack-test/stack/bin/validatorWorker.js --adapter=dummy --dummyIdentity=${id} >> /dev/null &`
-    const rSpawn = await spawn(cmd, {
+    const cmd = `DB_MONGO_NAME=${id} DB_MONGO_URL=${dbUrl || ''} nohup node /Users/Samparsky/Sites/nodejs/adex-validator-stack-test/stack/bin/validatorWorker.js --adapter=dummy --dummyIdentity=${id} >> ${id}.out`
+    const spawnR = await spawn(cmd, {
         stdio: 'inherit',
         shell: true,
         detached: true
     })
-
-    console.log({ rSpawn })
+    pids.push(spawnR.pid)
+    // console.log({ spawnR })
     // console.log({ stderr })
 }
 
@@ -121,12 +137,12 @@ async function setupLeader(){
     // run sentry
     console.log("setup sentry") 
 
-    await _setupSentry("awesomeLeader", "", port)
-    console.log('Taking a break...');
-    await sleep(2000);
-    console.log('Two seconds later');
-    // setup work
-    await _setupWorker("awesomeLeader", "")
+    // await _setupSentry("awesomeLeader", "", port)
+    // console.log('Taking a break...');
+    // await sleep(2000);
+    // console.log('Two seconds later');
+    // // setup work
+    // await _setupWorker("awesomeLeader", "")
     // send messages
     console.log("send messages") 
     await sendEvents(port)
@@ -140,12 +156,13 @@ async function setupFollower(){
     const port = 8006
 
     // run sentry
-    await _setupSentry("awesomeFollower", "", port)
-    console.log('Taking a break...');
-    await sleep(2000);
-    console.log('Two seconds later');
-    // // setup work
-    await _setupWorker("awesomeFollower", "")
+    // await _setupSentry("awesomeFollower", "", port)
+    // console.log('Taking a break...');
+    // await sleep(2000);
+    // console.log('Two seconds later');
+    // // // setup work
+    // await _setupWorker("awesomeFollower", "")
+    console.log("send messages") 
 
     await sendEvents(port)
 }
